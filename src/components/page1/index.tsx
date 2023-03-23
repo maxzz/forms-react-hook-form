d74b4ce98fb868deab95276aa5a832b000f6bc24
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-23 16:27:58 -0700

    WIP. add simplest use case

--- src/components/page1/index.tsx
+++ src/components/page1/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+    example: string,
+    exampleRequired: string,
+};
 
 export function Page1_Form() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+    console.log(watch("example"));
+
     return (
-        <div>index</div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+            <input defaultValue="test" {...register("example")} />
+
+
+            <input {...register("exampleRequired", { required: true })} />
+            {errors.exampleRequired &&
+                <span>This field is required</span>
+            }
+
+            <input type="submit" />
+        </form>
     );
 }
