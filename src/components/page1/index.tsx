5b88cabf4933f7f69052db38620e3ba0fa7bc287
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-24 13:38:25 -0700

    WIP. hook-form. fix controls layout

--- src/components/page1/index.tsx
+++ src/components/page1/index.tsx
@@ -20,27 +20,27 @@ function Form() {
     console.log(watch("example"));
 
     return (
-        <form className="p-4 border-yellow-500 border rounded grid gap-y-2" onSubmit={handleSubmit(onSubmit)}>
+        <form className="p-4 border-yellow-500 border rounded shadow grid gap-y-2" onSubmit={handleSubmit(onSubmit)}>
+            <div className="pb-4">Form caption</div>
 
             <div className="grid">
                 <input className="px-4 py-2 rounded" defaultValue="test" {...register("example", { required: true })} />
-                <span className={classNames("text-xs pb-4 text-red-500 select-none", !errors.example && 'invisible',)}>This field is required</span>
+                <span className={classNames("text-xs text-red-500 select-none", !errors.example && 'invisible',)}>This field is required</span>
             </div>
-
 
             <div className="grid">
                 <input className="px-4 py-2 rounded" {...register("exampleRequired", { required: true })} />
-                <span className={classNames("text-xs pb-4 text-red-500 select-none", !errors.exampleRequired && 'invisible',)}>This field is required</span>
+                <span className={classNames("text-xs text-red-500 select-none", !errors.exampleRequired && 'invisible',)}>This field is required</span>
             </div>
 
-            <input className="place-self-center px-2 py-2 border-slate-400 border rounded" type="submit" />
+            <input className="place-self-center mt-4 px-2 py-2 border-yellow-500 border rounded active:scale-y-[.97]" type="submit" />
         </form>
     );
 }
 
 export function Page1_Form() {
     return (
-        <div className="w-[420px] flex items-center justify-center bg-yellow-300">
+        <div className="w-[420px] flex items-center justify-center text-yellow-900 bg-yellow-300">
             <Form />
         </div>
     );
