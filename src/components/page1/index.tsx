ef6a9f19665acb7b85e24f19ff4eda840fb1f13a
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-23 19:12:45 -0700

    WIP. hook-form. add defaultValues

--- src/components/page1/index.tsx
+++ src/components/page1/index.tsx
@@ -7,25 +7,31 @@ type Inputs = {
     exampleRequired: string,
 };
 
+const defaultValues: Inputs = {
+    example: '123',
+    exampleRequired: 'required'
+};
+
 function Form() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({ defaultValues });
 
     const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
     console.log(watch("example"));
 
     return (
-        <form className="grid" onSubmit={handleSubmit(onSubmit)}>
+        <form className="grid gap-y-2" onSubmit={handleSubmit(onSubmit)}>
 
             <input defaultValue="test" {...register("example")} />
 
+            <div className="grid">
+                <input {...register("exampleRequired", { required: true })} />
 
-            <input {...register("exampleRequired", { required: true })} />
-
-            <span className={classNames(
-                "text-xs pb-4 text-red-500",
-                !errors.exampleRequired && 'invisible',
-            )}>This field is required</span>
+                <span className={classNames(
+                    "text-xs pb-4 text-red-500",
+                    !errors.exampleRequired && 'invisible',
+                )}>This field is required</span>
+            </div>
 
             <input className="px-2 py-1 border-slate-400 border rounded" type="submit" />
         </form>
@@ -34,7 +40,7 @@ function Form() {
 
 export function Page1_Form() {
     return (
-        <div className="px-4 min-w-[420px]">
+        <div className="px-4 py-3 max-w-[420px]">
             <Form />
         </div>
     );
