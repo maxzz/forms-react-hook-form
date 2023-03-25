4b98064249e8b7e2bdd0054c52e1cc12aedb8e8f
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-24 17:07:44 -0700

    WIP. hook-form. add first preview

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Page1_Form } from "./components/page1";
 export function App() {
     return (
         <div className="min-h-screen flex justify-center bg-yellow-200">
-            <div className="bg-green-500">11</div>
+            <div className="bg-green-500">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
             <Page1_Form />
         </div>
     );
