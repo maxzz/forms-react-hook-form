8a2e7ced11cd7e55e08dda5aa4845313ee411992
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-26 16:21:23 -0700

    WIP. hook-form3. add OtherControlsGroup w/ nested NamesGroup component

--- src/components/page2/test-form.tsx
+++ src/components/page2/test-form.tsx
@@ -1,5 +1,5 @@
 import { HTMLAttributes } from "react";
-import { FieldErrors, useForm, UseFormRegister } from "react-hook-form";
+import { FieldErrors, useForm, UseFormRegister, UseFormWatch } from "react-hook-form";
 import { classNames } from "../../utils/classnames";
 import { Input, Select, Radio, Button, DebugDisplay } from "./controls";
 import { Inputs, selectOptions } from "./controls-data";
@@ -11,6 +11,44 @@ function NamesGroup({ register, errors }: { register: UseFormRegister<Inputs>; e
     </>);
 }
 
+function OtherControlsGroup({ register, errors, watch }: { register: UseFormRegister<Inputs>; errors: FieldErrors<Inputs>; watch: UseFormWatch<Inputs>; }) {
+    const moreDetail = watch('moreDetails');
+    return (<>
+        {/* Simple inputs */}
+
+        <NamesGroup register={register} errors={errors} />
+
+        {/* Select */}
+
+        <label className="flex items-center space-x-1 select-none">
+            <span>Title</span>
+            <Select registered={register('title')} options={selectOptions} />
+        </label>
+
+        {/* Radio */}
+
+        <div className="my-2 flex items-center gap-x-4">
+            <Radio label="One" value="2" registered={register("radioIn", { required: true })} />
+            <Radio label="Two" value="3" registered={register("radioIn", { required: true })} />
+        </div>
+
+        {/* Optional fields */}
+
+        <label className="pt-2 flex items-center gap-x-2 select-none">
+            <input className="w-5 h-5 accent-yellow-900" type="checkbox" {...register('moreDetails')} />
+            <span>More controls..</span>
+        </label>
+
+        <div className={classNames(!moreDetail && 'invisible')}>
+            <label className="mt-1 grid">
+                <span>Optional data</span>
+                <input className="px-4 py-2 rounded" {...register('interests')} />
+            </label>
+        </div>
+
+    </>);
+}
+
 export function TestForm2({ defaultValues, className, ...rest }: { defaultValues: Inputs; } & HTMLAttributes<HTMLDivElement>) {
     const { register, handleSubmit, watch, reset, formState: { errors } } = useForm({ defaultValues });
 
@@ -19,7 +57,6 @@ export function TestForm2({ defaultValues, className, ...rest }: { defaultValues
     }
 
     const displayValues = watch();
-    const moreDetail = displayValues.moreDetails;
 
     return (
         <div className={classNames("px-4 pt-8 w-full h-full flex flex-col", className)} {...rest}>
@@ -31,37 +68,7 @@ export function TestForm2({ defaultValues, className, ...rest }: { defaultValues
                         <div className="px-4 py-2 text-xl font-semibold bg-yellow-500 rounded-t scale-y-110 tracking-tighter select-none">Form caption</div>
 
                         <div className="p-4">
-                            {/* Simple inputs */}
-
-                            <NamesGroup register={register} errors={errors} />
-
-                            {/* Select */}
-
-                            <label className="flex items-center space-x-1 select-none">
-                                <span>Title</span>
-                                <Select registered={register('title')} options={selectOptions} />
-                            </label>
-
-                            {/* Radio */}
-
-                            <div className="my-2 flex items-center gap-x-4">
-                                <Radio label="One" value="2" registered={register("radioIn", { required: true })} />
-                                <Radio label="Two" value="3" registered={register("radioIn", { required: true })} />
-                            </div>
-
-                            {/* Optional fields */}
-
-                            <label className="pt-2 flex items-center gap-x-2 select-none">
-                                <input className="w-5 h-5 accent-yellow-900" type="checkbox" {...register('moreDetails')} />
-                                <span>More controls..</span>
-                            </label>
-
-                            <div className={classNames(!moreDetail && 'invisible')}>
-                                <label className="mt-1 grid">
-                                    <span>Optional data</span>
-                                    <input className="px-4 py-2 rounded" {...register('interests')} />
-                                </label>
-                            </div>
+                            <OtherControlsGroup register={register} errors={errors} watch={watch} />
 
                             {/* Buttons */}
 
