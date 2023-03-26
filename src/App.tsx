6f84c66e6875ddd2a3974b0bacc40078f923fab0
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-26 15:32:46 -0700

    WIP. hook-form3. add #portals-container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Page1_Form } from "./components/page1SimplestForm";
 import { Page2_Form } from "./components/page2";
+import { Page3_Form } from "./components/page3";
 
 export function App() {
     return (
         <div className="min-h-screen flex justify-center bg-yellow-200">
             <div className="bg-green-500 w-8"></div>
-            <Page2_Form />
+            <Page3_Form />
         </div>
     );
 }
