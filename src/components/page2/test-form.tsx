d059bbb9dd4031ad1791d57db2e07fbcd39d7fb2
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-26 18:33:23 -0700

    WIP. hook-form3. ---------- add custom input w/ floating placeholders

--- src/components/page2/test-form.tsx
+++ src/components/page2/test-form.tsx
@@ -84,6 +84,63 @@ export function TestForm2({ defaultValues, className, onSave, onClose, ...rest }
                             Form caption
                         </div>
 
+                        {/* https://tailwind-elements.com/docs/standard/forms/registration-form */}
+                        {/* <div className="flex flex-wrap justify-center">
+                            <button
+                                type="button"
+                                className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                                data-te-toggle="tooltip"
+                                data-te-html="true"
+                                data-te-ripple-init
+                                data-te-ripple-color="light"
+                                title="<em>Tooltip</em> <u>with</u> <b>HTML</b>">
+                                Tooltip with html
+                            </button>
+                        </div>
+
+                        <div className="relative mb-6 bg-red-500" data-te-input-wrapper-init>
+                            <input
+                                type="text"
+                                className="peer block min-h-[auto] w-full rounded 
+                                border-0 
+                                bg-white 
+                                py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear 
+                                
+                                focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 
+                                
+                                motion-reduce:transition-none 
+                                dark:text-neutral-200 dark:placeholder:text-neutral-200 
+                                [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+
+                                id="exampleFormControlInput2"
+                                placeholder="--Email address" />
+                            <label
+                                htmlFor="exampleFormControlInput2"
+                                className="pointer-events-none 
+                                absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] 
+                                text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] 
+                                peer-focus:scale-[0.8] 
+                                peer-focus:text-primary 
+                                peer-data-[te-input-state-active]:-translate-y-[1.15rem] 
+                                peer-data-[te-input-state-active]:scale-[0.8] 
+                                motion-reduce:transition-none 
+                                dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+                            >Email address
+                            </label>
+                        </div> */}
+
+                        <label className="relative">
+                            <input
+                                className="py-1.5 w-full peer float-input border-slate-300 border"
+                                type="text"
+                                placeholder="placeholder"
+                            />
+                            <div className="float-label">
+                                placeholder
+                            </div>
+                        </label>
+
+
                         <div className="p-4">
                             <OtherControlsGroup register={register} errors={errors} watch={watch} />
 
