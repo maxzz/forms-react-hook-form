f36f166153f49ddf50987bb8475533950915c745
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-27 16:32:32 -0700

    WIP. hook-form3. add open/close functions

--- src/components/page3/index.tsx
+++ src/components/page3/index.tsx
@@ -1,15 +1,31 @@
-import { defaultValues } from "../page2/controls-data";
+import { defaultValues, Inputs } from "../page2/controls-data";
 import { TestForm2 } from "../page2/test-form";
 import { ReactPortal } from '../page2/controls';
+import { useState } from "react";
+import { classNames } from "../../utils/classnames";
 
 export function Page3_Form() {
-    function onClose() {
-        console.log('onClose');
+    const [open, setOpen] = useState(false);
+
+    function onSave(data: Inputs) {
+        console.log('dialog data', JSON.stringify(data, null, 4));
+        setOpen((p) => !p);
     }
+
+    function onClose() {
+        setOpen((p) => !p);
+    }
+
     return (
         <div className="flex items-center justify-center">
+            <div className="self-start  z-10">
+                <button className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95" onClick={onClose}>Open</button>
+            </div>
+
             <ReactPortal>
-                <TestForm2 defaultValues={defaultValues} onClose={onClose} />
+                <div className={classNames("h-full", !open && "hidden")}>
+                    <TestForm2 defaultValues={defaultValues} onSave={onSave} onClose={onClose} />
+                </div>
             </ReactPortal>
         </div>
     );
