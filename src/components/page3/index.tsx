3b1b5535ac97123b99b32c377e41d42338bc9c09
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-28 16:27:35 -0700

    WIP. app. failed to handle popup events

--- src/components/page3/index.tsx
+++ src/components/page3/index.tsx
@@ -17,19 +17,21 @@ export function Page3_Form() {
     }
 
     return (
-        <div className="flex items-center justify-center">
-            <div className="self-start  z-10">
-                <button
-                    className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95"
-                    onClick={onClose}
-                >{open ? "Close" : "Open"}</button>
-            </div>
-
-            <ReactPortal>
-                <div className={classNames("h-full", !open && "hidden")}>
-                    <TestForm2 defaultValues={defaultValues} onSave={onSave} onClose={onClose} />
+        // <div className="absolute w-full h-full inset-0 left-0 top-0 grid">
+            <div className="flex items-center justify-center pointer-events-auto">
+                <div className="self-start  z-10">
+                    <button
+                        className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95"
+                        onClick={onClose}
+                    >{open ? "Close" : "Open"}</button>
                 </div>
-            </ReactPortal>
-        </div>
+
+                <ReactPortal>
+                    <div className={classNames("h-full", !open && "hidden")}>
+                        <TestForm2 defaultValues={defaultValues} onSave={onSave} onClose={onClose} />
+                    </div>
+                </ReactPortal>
+            </div>
+        // </div>
     );
 }
