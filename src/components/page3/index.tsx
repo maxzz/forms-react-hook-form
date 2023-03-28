230cb39a32b529c0203d319bf2c7d49709c89167
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-28 16:35:45 -0700

    WIP. app. fix problem w/ handling popup events

--- src/components/page3/index.tsx
+++ src/components/page3/index.tsx
@@ -17,21 +17,54 @@ export function Page3_Form() {
     }
 
     return (
-        // <div className="absolute w-full h-full inset-0 left-0 top-0 grid">
-            <div className="flex items-center justify-center pointer-events-auto">
-                <div className="self-start  z-10">
-                    <button
-                        className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95"
-                        onClick={onClose}
-                    >{open ? "Close" : "Open"}</button>
-                </div>
+        <div className="flex items-center justify-center pointer-events-auto">
+            <div className="self-start  z-10">
+                <button
+                    className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95"
+                    onClick={onClose}
+                >{open ? "Close" : "Open"}</button>
+            </div>
 
-                <ReactPortal>
-                    <div className={classNames("h-full", !open && "hidden")}>
+            <ReactPortal>
+                <div className={classNames("h-full", !open && "hidden")}>
+                    {/* <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[1040]">             */}
+                    <div className="fixed inset-0 z-[1040]">
                         <TestForm2 defaultValues={defaultValues} onSave={onSave} onClose={onClose} />
                     </div>
-                </ReactPortal>
-            </div>
-        // </div>
+                </div>
+            </ReactPortal>
+        </div>
     );
 }
+
+// export function Page3_Form() {
+//     const [open, setOpen] = useState(false);
+
+//     function onSave(data: Inputs) {
+//         console.log('dialog data', JSON.stringify(data, null, 4));
+//         setOpen((p) => !p);
+//     }
+
+//     function onClose() {
+//         setOpen((p) => !p);
+//     }
+
+//     return (
+//         // <div className="absolute w-full h-full inset-0 left-0 top-0 grid">
+//             <div className="flex items-center justify-center pointer-events-auto">
+//                 <div className="self-start  z-10">
+//                     <button
+//                         className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95"
+//                         onClick={onClose}
+//                     >{open ? "Close" : "Open"}</button>
+//                 </div>
+
+//                 <ReactPortal>
+//                     <div className={classNames("h-full", !open && "hidden")}>
+//                         <TestForm2 defaultValues={defaultValues} onSave={onSave} onClose={onClose} />
+//                     </div>
+//                 </ReactPortal>
+//             </div>
+//         // </div>
+//     );
+// }
