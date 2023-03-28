0a08aecf40d1ce25ba83fc075d9e7632d21811f0
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-28 14:05:12 -0700

    WIP. hook-form3. update open/close action for Page3_Form

--- src/components/page3/index.tsx
+++ src/components/page3/index.tsx
@@ -19,7 +19,10 @@ export function Page3_Form() {
     return (
         <div className="flex items-center justify-center">
             <div className="self-start  z-10">
-                <button className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95" onClick={onClose}>Open</button>
+                <button
+                    className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95"
+                    onClick={onClose}
+                >{open ? "Close" : "Open"}</button>
             </div>
 
             <ReactPortal>
