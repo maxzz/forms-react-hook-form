25ff4981c5277ea1bfdb5c89e7ca24155043fb82
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-28 17:19:05 -0700

    WIP. ui. refactor icons

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -1,9 +1,23 @@
-export { ReactComponent as Vessel7 } from '../../../assets/sources/vessel7.svg';
+import { HTMLAttributes } from 'react';
+import { classNames } from '../../../utils/classnames';
 
-export function Star() {
+export { ReactComponent as IconVessel7 } from '../../../assets/sources/vessel7.svg';
+
+export function IconStar({ title, ...rest }: HTMLAttributes<SVGSVGElement>) {
     return (
-        <svg className="w-12 h-12 fill-slate-500 animate-in fade-in duration-1000" viewBox="-12 -12 24 24">
+        <svg viewBox="-12 -12 24 24" {...rest}>
+            {title && <title>{title}</title>}
             <path d="M -9 0 C 0 0 0 0 0 -9 C 0 0 0 0 9 0 C 0 0 0 0 0 9 C 0 0 0 0 -9 0" />
         </svg>
     );
 }
+
+export function IconClose({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-none stroke-current ", className)} viewBox="0 0 24 24" strokeWidth={1.5} {...rest}>
+            {title && <title>{title}</title>}
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    );
+}
+//stroke-[1.5]
