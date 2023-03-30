fc783bd85c816555bf3748b74ddb1c2fc657237a
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-30 15:18:57 -0700

    WIP. share form. fix vertical flow

--- src/components/page2/test-form.tsx
+++ src/components/page2/test-form.tsx
@@ -8,46 +8,48 @@ import { IconClose, IconStar, IconVessel7 } from "../ui/icons";
 function NamesGroup({ register, errors }: { register: UseFormRegister<Inputs>; errors: FieldErrors<Inputs>; }) {
     return (<>
         <InputFloat registered={register('name', { required: 'This field is required' })} errors={errors} placeholder="User name" />
-        <Input registered={register('firstName', { required: 'This field is required' })} errors={errors} />
-        <Input registered={register('lastName', { required: 'This field is required' })} errors={errors} />
+        <InputFloat registered={register('firstName', { required: 'This field is required' })} errors={errors} placeholder="First name" />
+        <InputFloat registered={register('lastName', { required: 'This field is required' })} errors={errors} placeholder="Last name" />
     </>);
 }
 
 function OtherControlsGroup({ register, errors, watch }: { register: UseFormRegister<Inputs>; errors: FieldErrors<Inputs>; watch: UseFormWatch<Inputs>; }) {
     const moreDetail = watch('moreDetails');
-    return (<>
-        {/* Simple inputs */}
+    return (
+        <div className="grid gap-2">
+            {/* Simple inputs */}
 
-        <NamesGroup register={register} errors={errors} />
+            <NamesGroup register={register} errors={errors} />
 
-        {/* Select */}
+            {/* Select */}
 
-        <label className="flex items-center space-x-1 select-none">
-            <span>Title</span>
-            <Select registered={register('title')} options={selectOptions} />
-        </label>
-
-        {/* Radio */}
-
-        <div className="my-2 flex items-center gap-x-4">
-            <Radio label="One" value="2" registered={register("radioIn", { required: true })} />
-            <Radio label="Two" value="3" registered={register("radioIn", { required: true })} />
-        </div>
-
-        {/* Optional fields */}
-
-        <label className="pt-2 flex items-center gap-x-2 select-none">
-            <input className="w-5 h-5 accent-yellow-900" type="checkbox" {...register('moreDetails')} />
-            <span>More controls..</span>
-        </label>
-
-        <div className={classNames(!moreDetail && 'invisible')}>
-            <label className="mt-1 grid">
-                <span>Optional data</span>
-                <input className="px-4 py-2 rounded" {...register('interests')} />
+            <label className="flex items-center space-x-1 select-none">
+                <span>Title</span>
+                <Select registered={register('title')} options={selectOptions} />
             </label>
+
+            {/* Radio */}
+
+            <div className="my-2 flex items-center gap-x-4">
+                <Radio label="One" value="2" registered={register("radioIn", { required: true })} />
+                <Radio label="Two" value="3" registered={register("radioIn", { required: true })} />
+            </div>
+
+            {/* Optional fields */}
+
+            <label className="pt-2 flex items-center gap-x-2 select-none">
+                <input className="w-5 h-5 accent-yellow-900" type="checkbox" {...register('moreDetails')} />
+                <span>More controls..</span>
+            </label>
+
+            <div className={classNames(!moreDetail && 'invisible')}>
+                <label className="mt-1 grid">
+                    <span>Optional data</span>
+                    <input className="px-4 py-2 rounded" {...register('interests')} />
+                </label>
+            </div>
         </div>
-    </>);
+    );
 }
 
 function LiveDebugDisplay({ watch, ...rest }: { watch: UseFormWatch<Inputs>; } & HTMLAttributes<HTMLDivElement>) {
@@ -78,6 +80,8 @@ export function TestForm2({ defaultValues, className, onSave, onClose, ...rest }
                 <div className="border-yellow-700 border rounded shadow overflow-hidden animate-in fade-in zoom-in ease-out duration-150">
                     <form className=" w-[420px] bg-yellow-400 grid gap-y-2" onSubmit={(e) => handleSubmitWithoutPropagation(e, true)}>
 
+                        {/* Caption */}
+
                         <div className="bg-yellow-500 flex items-center justify-between select-none">
                             <div className="px-4 py-2 flex items-center ">
                                 <div className="mr-4 py-2 text-xl font-semibold tracking-tighter scale-y-110 origin-bottom">Form caption</div>
@@ -85,9 +89,13 @@ export function TestForm2({ defaultValues, className, onSave, onClose, ...rest }
                                 <IconStar className="w-8 h-8 fill-slate-500 animate-in fade-in duration-1000" />
                             </div>
                             {onClose &&
-                                <div className="px-3 h-full hover:bg-gray-900/10 flex items-center" onClick={(e) => handleSubmitWithoutPropagation(e, false)}>
+                                <button
+                                    className="px-3 h-full hover:bg-gray-900/10 flex items-center"
+                                    title="Close dialog"
+                                    onClick={(e) => handleSubmitWithoutPropagation(e, false)}
+                                >
                                     <div className="w-6 h-6"><IconClose /></div>
-                                </div>
+                                </button>
                             }
                         </div>
 
@@ -100,7 +108,9 @@ export function TestForm2({ defaultValues, className, onSave, onClose, ...rest }
                                 <Button onClick={() => reset(defaultValues)}>Reset</Button>
                                 <Button type="submit">OK</Button>
 
-                                {onClose && <Button onClick={(e) => handleSubmitWithoutPropagation(e, false)}>Cancel</Button>}
+                                {onClose &&
+                                    <Button onClick={(e) => handleSubmitWithoutPropagation(e, false)}>Cancel</Button>
+                                }
                             </div>
                         </div>
                     </form>
@@ -111,3 +121,6 @@ export function TestForm2({ defaultValues, className, onSave, onClose, ...rest }
         </div>
     );
 }
+
+//TODO: click outside - now it is only modal
+//TODO: escape button
--- src/components/page3/index.tsx
+++ src/components/page3/index.tsx
@@ -19,11 +19,11 @@ export function Page3_Form() {
 
     return (
         <div className="flex items-center justify-center pointer-events-auto">
-            <div className="self-start  z-10">
+            <div className="self-start z-10">
                 <button
                     className="mx-4 mt-4 px-4 py-2 bg-gray-500/20 border-gray-500/50 border rounded shadow select-none active:scale-y-95"
                     onClick={onClose}
-                >{appUi.page3open ? "Close" : "Open"}</button>
+                >{appUi.page3open ? "Opened" : "Open"}</button>
             </div>
 
             <ReactPortal>
