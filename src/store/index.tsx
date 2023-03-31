8f2dc2f3fc95165dd607c630baf0b069d7d2c807
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-30 17:02:13 -0700

    WIP. store. add appUi persistance to localStorage

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,5 +1,10 @@
-import { proxy } from 'valtio';
+import { proxy, subscribe } from 'valtio';
 export { useSnapshot } from 'valtio';
+
+import { defaultValues, Form2Inputs } from '@/components/page2/controls-data';
+
+const STORAGE_KEY = 'forms-react-hook-form';
+const STORAGE_VER = 'v1';
 
 export const enum ActivePage {
     page1,
@@ -10,9 +15,31 @@ export const enum ActivePage {
 type AppUi = {
     activePage: ActivePage;
     page3open: boolean;
+    form2Inputs: Form2Inputs;
 };
 
-export const appUi = proxy<AppUi>({
+const initialAppUi: AppUi = {
     activePage: ActivePage.page3,
     page3open: true,
+    form2Inputs: defaultValues,
+};
+
+export const appUi = proxy<AppUi>(loadStorageAppUi());
+
+// Local storage
+
+function loadStorageAppUi(): AppUi {
+    const storage = localStorage.getItem(STORAGE_KEY);
+    if (storage) {
+        try {
+            const state = JSON.parse(storage);
+            return state[STORAGE_VER] || initialAppUi;
+        } catch (error) {
+        }
+    }
+    return initialAppUi;
+}
+
+subscribe(appUi, () => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ [STORAGE_VER]: appUi }));
 });
