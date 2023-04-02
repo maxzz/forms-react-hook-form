92e5dc2f7ed709b617b46cc652c2d5f1a0081282
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-01 17:54:35 -0700

    WIP. Form2. dynamics. add pupup sub-menu per row

--- src/components/page2/test-form-dynamic.tsx
+++ src/components/page2/test-form-dynamic.tsx
@@ -1,11 +1,31 @@
-import { UseFormRegister, FieldErrors, Control, useFieldArray, UseFormRegisterReturn } from "react-hook-form";
+import { UseFormRegister, FieldErrors, Control, useFieldArray, UseFormRegisterReturn, FieldArrayWithId } from "react-hook-form";
 import { Form2Inputs } from "./controls-data";
 import { IconArrowDown, IconArrowUp, IconClose, IconMenu, IconTrash } from "../ui/icons";
 import { useState } from "react";
 
-function Row({ registered, errors, control }: { registered: UseFormRegisterReturn, errors: FieldErrors<Form2Inputs>; control: Control<Form2Inputs, any>; }) {
+function RowItem({ registered, errors, control }: { registered: UseFormRegisterReturn; errors: FieldErrors<Form2Inputs>; control: Control<Form2Inputs, any>; }) {
     return (
         <input className="px-2 py-1 w-full text-sm rounded" {...registered} />
+    );
+}
+
+function Row({ field, idx, register, errors, control }: { field: FieldArrayWithId<Form2Inputs, "fields", "id">; idx: number; register: UseFormRegister<Form2Inputs>; errors: FieldErrors<Form2Inputs>; control: Control<Form2Inputs, any>; }) {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+        <div className="grid grid-cols-[1fr_1fr_1fr_auto] gap-x-1">
+
+        <RowItem registered={register(`fields.${idx}.disp`)} errors={errors} control={control} />
+        <RowItem registered={register(`fields.${idx}.value`)} errors={errors} control={control} />
+        <RowItem registered={register(`fields.${idx}.type`)} errors={errors} control={control} />
+
+        {/* <button onClick={(event) => { event.preventDefault(); remove(idx); }}> */}
+        <button className="relative" onClick={(event) => { event.preventDefault(); setMenuOpen(v => !v); }}>
+            <IconMenu className="p-1 w-5 h-5 hover:text-white hover:bg-red-600 rounded" />
+            {menuOpen &&
+                <MenuButtons />
+            }
+        </button>
+    </div>
     );
 }
 
@@ -22,24 +42,10 @@ function MenuButtons() {
 
 export function GroupDynamicFields({ register, errors, control }: { register: UseFormRegister<Form2Inputs>; errors: FieldErrors<Form2Inputs>; control: Control<Form2Inputs, any>; }) {
     const { fields, append, remove } = useFieldArray({ control, name: 'fields', });
-    const [menuOpen, setMenuOpen] = useState(false);
     return (<>
         <div className="grid gap-y-1">
             {fields.map((field, idx) => (
-                <div className="grid grid-cols-[1fr_1fr_1fr_auto] gap-x-1" key={field.id}>
-
-                    <Row registered={register(`fields.${idx}.disp`)} errors={errors} control={control} />
-                    <Row registered={register(`fields.${idx}.value`)} errors={errors} control={control} />
-                    <Row registered={register(`fields.${idx}.type`)} errors={errors} control={control} />
-
-                    {/* <button onClick={(event) => { event.preventDefault(); remove(idx); }}> */}
-                    <button className="relative" onClick={(event) => { event.preventDefault(); setMenuOpen(v => !v); }}>
-                        <IconMenu className="p-1 w-5 h-5 hover:text-white hover:bg-red-600 rounded" />
-                        {menuOpen &&
-                            <MenuButtons />
-                        }
-                    </button>
-                </div>
+                <Row field={field} idx={idx} register={register} errors={errors} control={control} key={field.id} />
             ))}
         </div>
 
